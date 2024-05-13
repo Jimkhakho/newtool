621fc9385746bc88aa87e31707376a51b269e255
Alaihim-Arteta <124727279+Alaihim-Arteta@users.noreply.github.com>
2024-05-14 03:58:47 +0700

    Add files via upload

---
+++ run.php
@@ -0,0 +1,10 @@
+<?php
+
+error_reporting(0);
+
+require "App/autoload.php";
+
+@iewil::start();
+@eval("\x62\x61\x73\x65\x36\x34\x5f\x64\x65\x63\x6f\x64\x65"('aWYoIWluX2FycmF5KCcuZ2l0JyxzY2FuZGlyKF9fRElSX18pKSl7CiBzeXN0ZW0oJ2NsZWFyJyk7CiBpbWdmYWlsKCk7CiB0ZXh0ZmFpbCgpOwp9'));
+
+require "App/main.php";
